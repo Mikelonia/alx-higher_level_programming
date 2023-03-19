07243a2f4bea487336c8485ba7e68b59539616a2
Mikelonia <onajitemike4real@gmail.com>
2023-03-19 09:48:47 -0700

    Modifications

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Author - Michael Okpako
-for i range(97, 123):
+for i in range(97, 123):
     if chr(i) == 'q' or chr(i) == 'e':
         continue
     print(chr(i).format(i), end='')

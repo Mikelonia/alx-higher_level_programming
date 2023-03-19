00b6ddfd4992c3a2210bfa33bd15d29d9e32fdda
Mikelonia <onajitemike4real@gmail.com>
2023-03-19 10:02:51 -0700

    Removal of trailing whitespace

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # Author - Michael Okpako
-
-for digit1 in range(0, 10):
+for digit1 in range(0, 8):
     for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
-        else:
-            print("{}{}".format(digit1, digit2), end=", ")
+         print("{:d}{:d}".format(digit1, digit2), end=', ')
+print("{:d}{:d}".format(digit1 + 1, digit2))

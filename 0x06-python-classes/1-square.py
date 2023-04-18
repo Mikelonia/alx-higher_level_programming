dddae98730be9967db834983e50de7e8bf88e557
Mikelonia <onajitemike4real@gmail.com>
2023-04-18 04:58:22 -0700

    Task 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining a class Square."""
+
+
+class Square:
+    """Representing a square."""
+
+    def __init__(self, size):
+        """Initializing a new Square.
+        Args:
+            size (int): The size of a new square.
+        """
+        self.__size = size 

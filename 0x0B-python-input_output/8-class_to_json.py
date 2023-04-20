9c624c599bb9a85758d83ac405046c3bfa625fca
Mikelonia <onajitemike4real@gmail.com>
2023-04-20 12:51:47 -0700

    Modified Task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,18 @@
-"""Defining a Python class-to-JSON function."""
-
-
 def class_to_json(obj):
-    """Returning the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    """Return a dictionary description of a simple object for JSON serialization."""
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("object has no attribute '__dict__'")
+
+    data = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, tuple)):
+            data[key] = [class_to_json(item) if hasattr(item, '__dict__') else item for item in value]
+        elif isinstance(value, dict):
+            data[key] = {k: class_to_json(v) if hasattr(v, '__dict__') else v for k, v in value.items()}
+        elif isinstance(value, (str, int, bool)):
+            data[key] = value
+        else:
+            data[key] = class_to_json(value) if hasattr(value, '__dict__') else None
+
+    return data
+

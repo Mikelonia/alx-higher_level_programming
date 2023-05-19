f22981479ec620f792021f6a071873947ca875ad
Mikelonia <onajitemike4real@gmail.com>
2023-05-19 08:20:06 -0700

    Task 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,11 +1,5 @@
 -- inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
 -- INSERT INTO table (columns) VALUES (values) -> Insert value to selected columns
-INSERT
-INTO first_table (
-    id,
-    name
-)
-VALUES (
-    89,
-    'Holberton School'
-)
+-- Insert a new row in the table first_table
+INSERT INTO `first_table` (id, name)
+VALUES (89, 'Best School');

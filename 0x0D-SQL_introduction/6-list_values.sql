9a4158380b0371280aabcd2edae837043d366d00
Mikelonia <onajitemike4real@gmail.com>
2023-05-19 08:13:07 -0700

    Task 6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+--  lists all rows of the table first_table from the database hbtn_0c_0 in my MySQL server.
+-- All fields will be printed
+-- SELECT * FROM tableName -> List all the rows of ALL columns, * is a wildcard denoting all columns
+SELECT * FROM first_table

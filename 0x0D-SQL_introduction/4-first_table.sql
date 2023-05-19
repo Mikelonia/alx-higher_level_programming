32ec6a08c1c1dad27e9435284904e1afa13ca4c7
Mikelonia <onajitemike4real@gmail.com>
2023-05-19 08:21:53 -0700

    Task 4 modification

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,17 +1,8 @@
 -- creates a table called first_table in the current database in my MySQL server.
 -- first_table description:id INT, name VARCHAR(256)
 -- If the table first_table already exists, my script should not fail
---#!/bin/bash
-
---# Get the database name as an argument
-database="$1"
-
---# SQL query to create the table
-query="CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);"
-
-# Execute the SQL query using the mysql command
-mysql -u username -p -e "$query" "$database"
-
+-- Create a table called first_table
+CREATE TABLE IF NOT EXISTS `first_table` (
+	`id` INT,
+	`name` VARCHAR(256)
+	);

82df8179688773759b53000dd00c4dba7cc2a083
Mikelonia <onajitemike4real@gmail.com>
2023-05-19 07:48:32 -0700

    Task 4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- creates a table called first_table in the current database in my MySQL server.
+-- first_table description:id INT, name VARCHAR(256)
+-- If the table first_table already exists, my script should not fail
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+);

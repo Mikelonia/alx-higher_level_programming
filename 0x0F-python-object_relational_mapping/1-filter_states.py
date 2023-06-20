a03230707b3c116f2cf6eb99feaac85037e35d05
Mikelonia <onajitemike4real@gmail.com>
2023-06-20 10:00:20 -0700

    Addition of LIMIT

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
-# by Okpako Michael
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa. By Okpako Michael.
+"""
+
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+def filter_states():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the query with WHERE clause to filter states starting with 'N' and ORDER BY states.id
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows and filter only 'New York' and 'Nevada'
+    rows = cur.fetchall()
+    filtered_rows = [(id, name) for id, name in rows if name == 'New York' or name == 'Nevada']
+    for row in filtered_rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+
+if __name__ == '__main__':
+    filter_states()
+

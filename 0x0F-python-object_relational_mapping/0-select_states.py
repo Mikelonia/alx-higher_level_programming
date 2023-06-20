51c596f960a81fe500fe5a1e5d2f53e0c6e15a5c
Mikelonia <onajitemike4real@gmail.com>
2023-06-20 09:39:08 -0700

    Addition of LIMIT

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,34 @@
 #                             <mysql password> \
 #                             <database name>
 # by Okpako Michael
-import sys
-import MySQLdb
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states` LIMIT 5")
-    [print(state) for state in c.fetchall()]
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the query with LIMIT 5 and ORDER BY states.id
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
+
+    # Fetch all the rows and print them
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
     # Close the cursor and database connection
-    c.close()
+    cur.close()
     db.close()
+

0ff12a5e067e5e8a9d7043261c7745899948a300
Mikelonia <onajitemike4real@gmail.com>
2023-06-20 09:42:45 -0700

    Addition of LIMIT

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-# by Okpako Michael
 """
 Script that lists all states from the database hbtn_0e_0_usa
 """
@@ -11,7 +6,7 @@ Script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+def select_states():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -34,4 +29,7 @@ if __name__ == '__main__':
     # Close the cursor and database connection
     cur.close()
     db.close()
+
+if __name__ == '__main__':
+    select_states()
 

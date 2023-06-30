0904095a1d6baac499c78c270dc7e59fffed5b64
Mikelonia <onajitemike4real@gmail.com>
2023-06-30 06:31:19 -0700

    Task  10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,7 +3,6 @@
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID. By Okpako Michael
 """
-
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
@@ -11,10 +10,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    response = requests.get("https://api.github.com/user", auth=auth)
-    data = response.json()
-    if "id" in data:
-        print(data["id"])
-    else:
-        print("Unable to retrieve GitHub ID.")
-
+    b = requests.get("https://api.github.com/user", auth=auth)
+    print(b.json().get("id"))

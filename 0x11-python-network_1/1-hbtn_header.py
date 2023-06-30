e33e95c112e56b6238e2abf3f6a530bf474aa8f5
Mikelonia <onajitemike4real@gmail.com>
2023-06-30 04:58:23 -0700

    Task 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 ''' Takes in URL, send a request to the URL and displays the value of
-the X-Request-Id variable found in the header of the response by Okpako Michael'''
+the X-Request-Id varibale found in the header of the response, by Okpako Michael '''
 
 import urllib.request
 import sys
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        headers = response.headers
-        x_request_id = headers.get('X-Request-Id')
-        print(x_request_id)
-
+try:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        location = response.headers.__dict__.get('_headers')
+        for i in location:
+            if 'X-Request-Id' in list(i):
+                print(i[1])
+except Exception:
+    pass

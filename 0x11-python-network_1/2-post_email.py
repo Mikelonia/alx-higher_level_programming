fa8d3c9212ede34f1b68ef1f3de1ebc879764dea
Mikelonia <onajitemike4real@gmail.com>
2023-06-30 05:11:04 -0700

    Task 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Displays the value of the X-Request-Id variable found in
+the header of the response. By Okpako Michael
+"""
+
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == "__main__":
+    value = {"email": argv[2]}
+    request = Request(argv[1], urlencode(value).encode("ascii"))
+
+    with urlopen(request) as response:
+        head = response.headers.get('X-Request-Id')
+        print(response.read().decode('utf-8'))
+

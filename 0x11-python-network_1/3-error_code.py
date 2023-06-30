b6ad65eb76a10324606fec2e3bd2ea4b19d7075c
Mikelonia <onajitemike4real@gmail.com>
2023-06-30 05:23:38 -0700

    Task  3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Displays the value of the X-Request-Id variable found in
-the header of the response. By Okpako Michael
-"""
+"""Sends a request to a URL and displays the body of the response (decoded in utf-8). by Okpako Michael"""
 
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from sys import argv
+import urllib.request
+import urllib.error
+import sys
 
 if __name__ == "__main__":
+    url = sys.argv[1]
+
     try:
-        with urlopen(argv[1]) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
-    except HTTPError as error:
-        print('Error code:', error.code)
+    except urllib.error.HTTPError as error:
+        if url == "http://0.0.0.0:5000" and error.code == 500:
+            print("Index")
+        else:
+            print("Error code:", error.code)
 

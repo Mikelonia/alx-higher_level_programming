7faacd97ad3e10399d808120b46e87fac065ee3d
Mikelonia <onajitemike4real@gmail.com>
2023-06-30 04:56:39 -0700

    Task 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,12 +6,9 @@ import urllib.request
 import sys
 
 if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-        with urllib.request.urlopen(url) as response:
-            headers = response.headers
-            x_request_id = headers.get('X-Request-Id')
-            print(x_request_id)
-    except Exception:
-        pass
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
+        x_request_id = headers.get('X-Request-Id')
+        print(x_request_id)
 
